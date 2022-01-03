c16c550ede8d9ac4bf77275b36f4afe41bac5620
capricious-slingshot <ephemere@protonmail.com>
2022-01-02 18:50:35 -0800

    adding calzoni data

---
+++ src/data/calzoni.js
@@ -0,0 +1,54 @@
+export const calzoniData = [
+  {
+    sectionDesc: "All Calzoni is made with Tomato Sauce and Mozzerella. Subtractions OK, no substitutions",
+    section1: null,
+    subheader1: null,
+    section1Items: [
+      {
+        itemName: "Pancrazio",
+        itemDesc: "Italian Sausage, Mushrooms",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Benedetto",
+        itemDesc: "Spicy Calabrese Sausage, Spicy Peppers",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Cipriano",
+        itemDesc: "Pepperoni",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Stefano",
+        itemDesc: "Ricotta cheese",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Pietro",
+        itemDesc: "Chicken sausage, Calabrian Peppers, Ricotta Cheese",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Gennaro",
+        itemDesc: "Meatballs, Onion, Peppers",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Claudio",
+        itemDesc: "Eggplant, Ricotta Cheese",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Filipo",
+        itemDesc: "Portabella Mushroom, Ricotta Cheese",
+        itemPrice: "11"
+      },
+      {
+        itemName: "Giacomo",
+        itemDesc: "Tomatoes, Onions, Olives, Ricotta Cheese",
+        itemPrice: "11"
+      },
+    ]
+  }
+]

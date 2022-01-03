1f04408a69d58b2c7c8f763a8f83f7a2c4726fa5
capricious-slingshot <ephemere@protonmail.com>
2022-01-02 19:05:26 -0800

    adding in nabev data

---
+++ src/data/naDrinks.js
@@ -0,0 +1,49 @@
+export const naDrinksData = [
+  {
+    sectionDesc: null,
+    section1: null,
+    subheader1: null,
+    section1Items: [
+      {
+        itemName: "Ginger Beer",
+        itemDesc: null,
+        itemPrice: "3.5"
+      },
+      {
+        itemName: "San Pellegrino",
+        itemDesc: null,
+        itemPrice: "3"
+      },
+      {
+        itemName: "Coke",
+        itemDesc: null,
+        itemPrice: "3"
+      },
+      {
+        itemName: "Diet Coke",
+        itemDesc: null,
+        itemPrice: "3"
+      },
+      {
+        itemName: "Fanta",
+        itemDesc: null,
+        itemPrice: "3"
+      },
+      {
+        itemName: "Sprite",
+        itemDesc: null,
+        itemPrice: "3"
+      },
+      {
+        itemName: "Ginger Ale",
+        itemDesc: null,
+        itemPrice: "3"
+      },
+      {
+        itemName: "Root Beer",
+        itemDesc: null,
+        itemPrice: "3"
+      }
+    ]
+  }
+]

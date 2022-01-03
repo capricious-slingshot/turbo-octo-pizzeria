126c7b7b939503376b9429586b77d55e9b685f96
capricious-slingshot <ephemere@protonmail.com>
2022-01-02 18:20:05 -0800

    adding in rough data for pizza menu section

---
+++ src/data/pizzas.js
@@ -0,0 +1,63 @@
+//questions - file name singular or plural? what is the best file tree structure here?
+
+export const pizzaData = [
+  {
+    sectionDesc: "Our handcrafted brick oven pizza is traditional 'dark crust' because it is baked directly on stone. If you prefer 'light crust' please specify. 10 inch is 4 slices and makes a great personal size, or a 16 inch is 8 slices, good for 2+ people.",
+    section1 : "House Combinations",
+    subheader1: '10" / 16"',
+    section1Items: [
+      {
+        itemName: "Calorosa",
+        itemDesc: "Tomato Sauce, Smoked Mozzerella, Pepperoni, Bacon, Italian Sausage",
+        itemPrice: "15 / 23"
+      },
+      {
+        itemName: "Bambinos",
+        itemDesc: "Tomato Sauce, Mozzerella, Pepperoni, Meatball, Mushrooms, Olives",
+        itemPrice: "15 / 24"
+      },
+      {
+        itemName: "Capricciosa",
+        itemDesc: "Tomato Sauce, Fresh Mozzerella, Italian Sausage, Onions, Mushrooms",
+        itemPrice: "14 / 20"
+      },
+      {
+        itemName: "Bufala",
+        itemDesc: "Tomato Sauce, Water Buffalo Mozzerella, Basil, Olive Oil",
+        itemPrice: "11 / 18"
+      },
+      {
+        itemName: "Genovese",
+        itemDesc: "Tomato Sauce, Smoked Mozzerella, Pepperoni, Bacon, Italian Sausage",
+        itemPrice: "15 / 23"
+      },
+      {
+        itemName: "Lazio",
+        itemDesc: "Tomato Sauce, Smoked Mozzerella, Pepperoni, Bacon, Italian Sausage",
+        itemPrice: "15 / 23"
+      },
+      {
+        itemName: "Tropicale",
+        itemDesc: "Tomato Sauce, Smoked Mozzerella, Pepperoni, Bacon, Italian Sausage",
+        itemPrice: "15 / 23"
+      },
+      {
+        itemName: "Primavera",
+        itemDesc: "Tomato Sauce, Smoked Mozzerella, Pepperoni, Bacon, Italian Sausage",
+        itemPrice: "15 / 23"
+      }],
+    section2: "Build Your Own",
+    section2Items: [
+      {
+        itemName: "Margherita Base",
+        itemDesc: "Tomato Sauce, Fresh Mozzerella, Basil",
+        itemPrice: "11 / 16"
+      },
+      {
+        itemName: "Aditional Toppings",
+        itemDesc: "Eggplant, Onions, Tomatoes, Pineapple, Olives, Gaeta Olives, Peppers,Castelvetrano Olives, Mushroom, Portebello, Roasted Garlic, Ricotta Mama's Lil' Peppers, Calabrian Peppers,Chicken, Pepperoni, Chicken Sausage, Italian Sausage, Spicy Calabrese Sausage, Ham, Salami, Bacon White Anchovies, Fontina, Extra Mozzerella",
+        itemPrice: "1.5 / 2.5"
+      }
+    ]
+  }
+]

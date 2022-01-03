07a54b6570be5461f3870c9736dd1ec1f72a47f4
capricious-slingshot <ephemere@protonmail.com>
2022-01-02 19:00:11 -0800

    addin salads data

---
+++ src/data/salads.js
@@ -0,0 +1,29 @@
+export const saladsData = [
+  {
+    sectionDesc: null,
+    section1: null,
+    subheader1: null,
+    section1Items: [
+      {
+        itemName: "Insalata Verde",
+        itemDesc: "Balsamic Vinegrette, Baby Greens",
+        itemPrice: "6"
+      },
+      {
+        itemName: "Insalata Cesarina",
+        itemDesc: "Caesar Dressing, Romaine Hearts, Croutons, Parmesan",
+        itemPrice: "9"
+      },
+      {
+        itemName: "Insalata Viola",
+        itemDesc: "Sherry Vinagrette, Baby Greens, Roasted Beets, Riocotta Salata",
+        itemPrice: "9"
+      },
+      {
+        itemName: "Insalata di Noci",
+        itemDesc: "Fig Vinagrette, Baby Greens, Candied Walnuts, Dried Cranberries, Chevre",
+        itemPrice: "10"
+      }
+    ]
+  }
+]

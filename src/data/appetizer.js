63850fc3bc461d4cfa0e3615de28f64416af06a9
capricious-slingshot <ephemere@protonmail.com>
2022-01-02 18:56:03 -0800

    addin appatiser data

---
+++ src/data/appetizer.js
@@ -0,0 +1,34 @@
+export const appetizerData = [
+  {
+    sectionDesc: null,
+    section1: null,
+    subheader1: null,
+    section1Items: [
+      {
+        itemName: "Assorted Olives",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+      {
+        itemName: "Cheese Plate",
+        itemDesc: "Flatbread, Honey, Brie and Beechers Cheese",
+        itemPrice: "10"
+      },
+      {
+        itemName: "Meatballs",
+        itemDesc: "Trio of beef meatballs in a marinara sauce. Served with breadsticks",
+        itemPrice: "10"
+      },
+      {
+        itemName: "Pork Belly",
+        itemDesc: "Braised Porkbelly served with garlic mustard and breadsticks",
+        itemPrice: "10"
+      },
+      {
+        itemName: "Breadsticks",
+        itemDesc: "2 large breadsticks served with marinara on the side",
+        itemPrice: "4"
+      },
+    ]
+  }
+]

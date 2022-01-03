d4ebbfbbc18de4d3d4760ee0153a693b11083701
capricious-slingshot <ephemere@protonmail.com>
2022-01-02 19:02:27 -0800

    addin gelato data

---
+++ src/data/gelato.js
@@ -0,0 +1,39 @@
+export const gelatoData = [
+  {
+    sectionDesc: "Price per Scoop",
+    section1: null,
+    subheader1: null,
+    section1Items: [
+      {
+        itemName: "Chocolate",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+      {
+        itemName: "Chocolate Hazelnut",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+      {
+        itemName: "Salted Carmel",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+      {
+        itemName: "Tiramisu",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+      {
+        itemName: "Coconut",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+      {
+        itemName: "Vanilla",
+        itemDesc: null,
+        itemPrice: "6"
+      },
+    ]
+  }
+]

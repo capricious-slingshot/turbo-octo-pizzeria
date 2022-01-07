6e722bafe62062065a97b0f7d286a86699ddfb8a
capricious-slingshot <ephemere@protonmail.com>
2022-01-06 21:57:35 -0800

    extracting menu layouts into card menu layout components

--- src/componets/Accordion.js
+++ src/componets/Accordion.js
@@ -1,7 +1,5 @@
 import { Component } from 'react'
 import Card from './Card'
-import Table from './Table'
-//question about dumbing down the card component - overlay and data-target should not be editable
 
 class Accordion extends Component {
   render() {
@@ -11,7 +9,7 @@ class Accordion extends Component {
           dataTarget="pizza"
           overlay="35"
           description="Our handcrafted brick oven pizza is traditional 'dark crust' because it is baked directly on stone. If you prefer 'light crust' please specify. 10 inch is 4 slices and makes a great personal size, or a 16 inch is 8 slices, good for 2+ people. "/>
-        <Table title="Microbrews"
+        <Card title="Microbrews"
           dataTarget="taps"
           overlay="10"
           description="All drafts $7" />
@@ -19,23 +17,23 @@ class Accordion extends Component {
           dataTarget="calzoni"
           overlay="50"
           description="All Calzoni is made with Tomato Sauce and Mozzerella. Subtractions OK, no substitutions" />
-      <Card title="Appetizers"
+        <Card title="Appetizers"
           dataTarget="appetizers"
           overlay="35"
           description="" />
-      <Card title="Salads"
+        <Card title="Salads"
           dataTarget="salad"
           overlay="50"
           description="" />
-      <Card title="Wine"
+        <Card title="Wine"
           dataTarget="vino"
           overlay="50"
           description="" />
-      <Card title="D'Ambrosio Gelato"
+        <Card title="D'Ambrosio Gelato"
           dataTarget="gelato"
           overlay="35"
           description="Price per Scoop" />
-      <Card title="Non Alcholic"
+        <Card title="Non Alcholic"
           dataTarget="na-bev"
           overlay="35"
           description=""/>
--- src/componets/Card.js
+++ src/componets/Card.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
-import CardItem from './CardItem'
+import CardMenuMain from './CardMenuMain'
+import CardMenuTable from './CardMenuTable'
 
 class Card extends Component{
   constructor(props) {
@@ -11,38 +12,18 @@ class Card extends Component{
   }
 
   render() {
+    let menuType = (this.dataTarget === 'taps') ? <CardMenuTable /> : <CardMenuMain />
+    let showSection = (this.dataTarget === 'pizza') ? "show" : ""
+
     return (
       <div className="card">
         <div className="card-header">
           <h4><button class="card-accordion-anchor" data-toggle="collapse" data-target={`#${this.dataTarget}`}>{ this.title }</button></h4>
-          <div className="collapse show" id={ this.dataTarget }data-parent="#accordion">
+          <div className={`collapse ${showSection}`} id={ this.dataTarget }data-parent="#accordion">
             <div className="card-body col-12 overflow" id={`#${this.dataTarget}-card`}>
               <div className={`cotainer overlay-${this.dataTarget} box-padding`}>
-                <p>{ this.description }</p>
-                <div className="row section-title">
-                  <div className="col">
-                    <h5>House Combinations</h5>
-                  </div>
-                  <div className="col-4 col-md-3 col-lg-2" >
-                    <h5 className="sizing">10" / 16"</h5>
-                  </div>
-                </div>
-                <CardItem name="Calorosa" description="Tomato Sauce, Smoked Mozzerella, Pepperoni, Bacon, Italian Sausage" price="15 / 23" />
-                <CardItem name="Bambinos" description="Tomato Sauce, Mozzerella, Pepperoni, Meatball, Mushrooms, Olives" price="15 / 24" />
-                <CardItem name="Capricciosa" description="Tomato Sauce, Fresh Mozzerella, Italian Sausage, Onions, Mushrooms" price="14 / 20" />
-                <CardItem name="Bufala" description="Tomato Sauce, Water Buffalo Mozzerella, Basil, Olive Oil" price="11 / 18" />
-                <CardItem name="Genovese" description="Pesto, Fresh Mozzerella, Chicken, Roasted Garlic, Tomatoes" price="14 / 22" />
-                <CardItem name="Lazio" description="Truffle Oil, Fontina, Mushrooms" price="13 / 20" />
-                <CardItem name="Tropicale" description="Tomato Sauce, Fresh Mozzerella, Canadian Bacon, Pineapple" price="13 / 20" />
-                <CardItem name="Primavera" description="Tomato Sauce, Fresh Mozzerella, Onions, Peppers, Mushrooms, Olives, Tomatoes" price="15 / 24" />
-                <div className="row section-title">
-                  <div className="col">
-                    <h5>Build Your Own</h5>
-                  </div>
-                </div>
-                <CardItem name="Margherita Base" description="Tomato Sauce, Fresh Mozzerella, Basil" price="11 / 16" />
-                <CardItem name="Aditional Toppings" description="Eggplant, Onions, Tomatoes, Pineapple, Olives, Gaeta Olives, Peppers,Castelvetrano Olives, Mushroom, Portebello, Roasted Garlic, Ricotta
-                      Mama's Lil' Peppers, Calabrian Peppers,Chicken, Pepperoni, Chicken Sausage, Italian Sausage, Spicy Calabrese Sausage, Ham, Salami, Bacon White Anchovies, Fontina, Extra Mozzerella" price="1.5 / 2.5" />
+                <p>{this.description}</p>
+                {menuType}
               </div>
             </div>
           </div>

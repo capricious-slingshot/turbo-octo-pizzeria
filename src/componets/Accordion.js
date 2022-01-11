3e6a4f706af82c42fbc0f6ead5ddfcc2cc09fbb5
capricious-slingshot <ephemere@protonmail.com>
2022-01-10 20:51:25 -0800

    I hate javascript

--- src/componets/Accordion.js
+++ src/componets/Accordion.js
@@ -1,60 +1,37 @@
 import { Component } from 'react'
 import Card from './Card'
 
+//is there a classier alternative to console.log? it's awful
+
+//pass to children
+  //issues with child consuming data - can't hit props. it just barfs.
+  //how can i even look at props ?
+
 class Accordion extends Component {
+  constructor() {
+    super()
+    this.pizzas = this.fetchData("pizzas")
+    this.microbrews = this.fetchData("microbrews")
+    this.appetizers = this.fetchData("appetizers")
+    this.calzoni = this.fetchData("calzoni")
+    this.salads = this.fetchData("salads")
+    this.wines = this.fetchData("wines")
+    this.gelato = this.fetchData("gelato")
+    this.nonalcholic = this.fetchData("nonalcholic")
+  }
+
+  fetchData(route) {
+    const baseUrl = 'http://localhost:3001/'
+    return fetch(baseUrl+route).then(response => response.json()).catch(err => console.log('There was an error:' + err))
+  }
+
   render() {
-    let data = fetch('http://localhost:3001/pizzas')
-               .then(response => response.json())
-               .then(data => console.log(data));
-    
-    //I don't want to make 8 seperate calls to the server - very expensive
-    //why can't I have a function outside of render but inside the class? scope?
-    //why was JS written by hipster idiots? I hate this language.
-
-
-    //data is a hash of hashes
-    //grab each key, and pass it's value to Card
-    //why can't I chain this shit? 
     return (
       <div className="accordion accordion-flush" id="accordion">
-        {console.log(data)}
-        {/* {Object.keys(data).forEach((key, index) => <Card data={data[key]} />)}  */}
+        {<Card data={this.pizzas} />}
       </div>
     )
   }
- }
+}
 
 export default Accordion
- 
-//   <Card data="Brick Oven Pizza"
-      //     dataTarget="pizza"
-      //     overlay="35"
-      //     description="Our handcrafted brick oven pizza is traditional 'dark crust' because it is baked directly on stone. If you prefer 'light crust' please specify. 10 inch is 4 slices and makes a great personal size, or a 16 inch is 8 slices, good for 2+ people. "/>
-      //   <Card title="Microbrews"
-      //     dataTarget="taps"
-      //     overlay="10"
-      //     description="All drafts $7" />
-      //   <Card title="Calzoni"
-      //     dataTarget="calzoni"
-      //     overlay="50"
-      //     description="All Calzoni is made with Tomato Sauce and Mozzerella. Subtractions OK, no substitutions" />
-      //   <Card title="Appetizers"
-      //     dataTarget="appetizers"
-      //     overlay="35"
-      //     description="" />
-      //   <Card title="Salads"
-      //     dataTarget="salad"
-      //     overlay="50"
-      //     description="" />
-      //   <Card title="Wine"
-      //     dataTarget="vino"
-      //     overlay="50"
-      //     description="" />
-      //   <Card title="D'Ambrosio Gelato"
-      //     dataTarget="gelato"
-      //     overlay="35"
-      //     description="Price per Scoop" />
-      //   <Card title="Non Alcholic"
-      //     dataTarget="na-bev"
-      //     overlay="35"
-      //     description=""/>
--- src/componets/Card.js
+++ src/componets/Card.js
@@ -3,26 +3,20 @@ import CardMenuMain from './CardMenuMain'
 import CardMenuTable from './CardMenuTable'
 
 class Card extends Component{
-  constructor(props) {
-    super(props)
-    this.dataTarget = props.dataTarget
-    this.overlay = props.overlay
-    this.title = props.title
-    this.description = props.description
-  }
 
   render() {
-    let menuType = (this.dataTarget === 'taps') ? <CardMenuTable /> : <CardMenuMain />
-    let showSection = (this.dataTarget === 'pizza') ? "show" : ""
-
+    let menuType = (this.props.data.dataTarget === 'taps') ? <CardMenuTable data={this.props.data} /> : <CardMenuMain data={this.props.data} />
+    let showSection = (this.props.data.dataTarget === 'pizza') ? "show" : ""
+    console.log(this.props.data) //can see promise in browser console
+    //all calls to this.props.data.... below just return undefined
     return (
       <div className="card">
         <div className="card-header">
-          <h4><button className="card-accordion-anchor" data-toggle="collapse" data-target={`#${this.dataTarget}`}>{ this.title }</button></h4>
-          <div className={`collapse ${showSection}`} id={ this.dataTarget }data-parent="#accordion">
-            <div className="card-body col-12 overflow" id={`#${this.dataTarget}-card`}>
-              <div className={`cotainer overlay-${this.dataTarget} box-padding`}>
-                <p>{this.description}</p>
+          <h4><button className="card-accordion-anchor" data-toggle="collapse" data-target={`#${this.props.data.dataTarget}`}>{ this.props.data.title }</button></h4>
+          <div className={`collapse ${showSection}`} id={ this.props.data.dataTarget } data-parent="#accordion">
+            <div className="card-body col-12 overflow" id={`#${this.props.data.dataTarget}-card`}>
+              <div className={`cotainer overlay-${this.props.data.dataTarget} box-padding`}>
+                <p>{this.props.description}</p>
                 {menuType}
               </div>
             </div>

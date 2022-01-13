527a8bb7ec6b55eed71eca04dc39d7e2c331e75a
capricious-slingshot <ephemere@protonmail.com>
2022-01-13 00:59:10 -0800

    cleaning up notes and comments

--- src/componets/Accordion.js
+++ src/componets/Accordion.js
@@ -1,21 +1,7 @@
 import { Component } from 'react'
 import Card from './Card'
 
-//   render() {
-//     if (this.state.isFetching) {
-//       return <div>Fetching Menu....</div>;
-//     }
-//     return (
-//       <div className="accordion accordion-flush" id="accordion">
-//         {<Card data={this.state.pizzas} />}
-//       </div>
-//     )
-//   }
-// }
-
-
 class Accordion extends Component {
-
   state = {
     isFetching: false,
     menu: {}
@@ -26,26 +12,25 @@ class Accordion extends Component {
     try {
       const response = await fetch(`http://localhost:3001/db`).then(resp => resp.json()).then(json => { this.setState({menu: json}) })
       if (!response.ok) {
-        throw Error(response.statusText);
+        throw Error(response.statusText)
       }
     } catch (error) {
-      console.log(`Error: ${error}`);
+      console.log(`Error: ${error}`)
     }
     this.setState({ isFetching: false })
-  } 
-  
+  }
+
   componentDidMount() {
     //doesn't this get called *after* render?
     //won't this be called every time the page is reloaded?
-    this.fetchData()
-    // if (Object.keys(this.state.menu).length !== 0) { this.fetchData() }
+    if (Object.keys(this.state.menu).length === 0) { this.fetchData() }
   }
 
   render() {
     if (this.state.isFetching) {
       return <div>Fetching Menu....</div>;
     }
-    //when state updates, triggers re-render automatically
+    //when state updates via setState , triggers re-render automatically
     return (
       <div className="accordion accordion-flush" id="accordion">
         {Object.keys(this.state.menu).map((section, key) => <Card key={ key } data={this.state.menu[section]} /> )}
